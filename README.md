36cd2badb95267ae2eab5afaf221765e976a20fd
vkajen <kvelummaylum@appcelerator.com>
2014-08-11 16:47:55 +0800

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This project demonstrate the automation of some basic features of IDE using Q7 automation tool and Sikuli scripting. `
---
+++ helloworld.sikuli/helloworld.py
@@ -0,0 +1,31 @@
+click("1407221752719.png")
+if(exists("1407221783068.png") == None):
+    Region.setFindFailedResponse();
+click("1407221812214.png")
+if(exists("1407221842948.png") == None):
+    Region.setFindFailedResponse();
+
+click("1407221752719.png")
+if(exists("1407221783068.png") == None):
+    Region.setFindFailedResponse();
+
+
+click("1407221812214.png")
+if(exists("1407221842948.png") == None):
+    Region.setFindFailedResponse();
+
+click("1407221752719.png")
+if(exists("1407221783068.png") == None):
+    Region.setFindFailedResponse();
+
+click("1407221812214.png")
+if(exists("1407221842948.png") == None):
+    Region.setFindFailedResponse();
+
+click("1407221752719.png")
+if(exists("1407221783068.png") == None):
+    Region.setFindFailedResponse();
+
+
+
+
